7075076680a5e1760e82cd1176fe5ebcc2f2a80f
Little-Captain <littlecaptain@foxmail.com>
2017-04-25 22:02:47 +0800

    项目的基本配置 1. 目录结构 2. App图标 3. App启动图片 4. 不使用storyboard 5. App的bundle display name

--- BSProject/BSProject/AppDelegate.m
+++ BSProject/BSProject/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "ViewController.h"
 
 @interface AppDelegate ()
 
@@ -16,7 +17,16 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    
+    // 创建窗口
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    
+    // 设置窗口的根控制器
+    self.window.rootViewController = [[ViewController alloc] init];
+    
+    // 显示窗口
+    [self.window makeKeyAndVisible];
+    
     return YES;
 }
 
--- BSProject/BSProject/ViewController.m
+++ BSProject/BSProject/ViewController.m
@@ -16,13 +16,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
-
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
+    self.view.backgroundColor = [UIColor yellowColor];
 }
 
 
