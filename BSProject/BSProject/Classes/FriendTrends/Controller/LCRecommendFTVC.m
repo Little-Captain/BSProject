fdcb9dce5195851b2a22e87e928a77215beeb42f
Little-Captain <littlecaptain@foxmail.com>
2017-04-26 10:36:38 +0800

    关注模块的主界面搭建 安装一些第三方框架

---
+++ BSProject/BSProject/Classes/FriendTrends/Controller/LCRecommendFTVC.m
@@ -0,0 +1,21 @@
+//
+//  LCRecommendFTVC.m
+//  BSProject
+//
+//  Created by Liu-Mac on 4/26/17.
+//  Copyright © 2017 Liu-Mac. All rights reserved.
+//
+
+#import "LCRecommendFTVC.h"
+
+@interface LCRecommendFTVC ()
+
+@end
+
+@implementation LCRecommendFTVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+@end
